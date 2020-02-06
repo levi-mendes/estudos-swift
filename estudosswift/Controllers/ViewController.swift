7f11e6ad538589f74d7eb1ed6b8fea5ccfa6514a
Levi Mendes <levi.mendes@foursys.com.br>
2020-02-06 08:57:02 -0300

    estudos "guard let" e ïf let"

--- estudosswift/Controllers/ViewController.swift
+++ estudosswift/Controllers/ViewController.swift
@@ -15,10 +15,39 @@ class ViewController: UIViewController {
     
     //version controller test
     @IBAction func adicionar(_ sender: Any) {
-        let nome = nomeTextField?.text
-        let felicidade = felicidadeTextField?.text
-            
-         print("Comi \(nome) e fiquei com felicidade \(felicidade)")
+        
+        /*
+        //tenta pegar os TextField's, so vai lancar exeption se eles nao existirem
+        if let nome = nomeTextField?.text, let felicidade = felicidadeTextField?.text {
+        
+            if let valorFelicidade = Int(felicidade) {
+                let refeicao = Refeicao(nome, valorFelicidade)
+                print("Comi \(refeicao.nome) e fiquei com felicidade \(refeicao.felicidade)")
+                
+            } else {
+                print("erro ao criar a refeicao")
+            }
+        }
+         */
+        
+        //other way
+        guard let nome = nomeTextField?.text else {
+            //TODO:
+            return
+        }
+        
+        guard let felicidade = felicidadeTextField?.text else {
+            //TODO:
+            return
+        }
+        
+        guard let valorFelicidade = Int(felicidade) else {
+            //TODO:
+            return
+        }
+        
+        let refeicao = Refeicao(nome, valorFelicidade)
+        print("Comi \(refeicao.nome) e fiquei com felicidade \(refeicao.felicidade)")
     }
     
 }
